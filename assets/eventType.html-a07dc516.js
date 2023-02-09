6194aceba0159812615e8c93cb58e67f9ea013f9
Mashiro <1526566980@qq.com>
2023-02-09 16:24:33 +0800

    update

---
+++ assets/eventType.html-a07dc516.js
@@ -0,0 +1,18 @@
+import{_ as n,p as s,q as a,a1 as e}from"./framework-204010b2.js";const t={},o=e(`<h2 id="typescript-中-event-事件类型" tabindex="-1"><a class="header-anchor" href="#typescript-中-event-事件类型" aria-hidden="true">#</a> TypeScript 中 Event 事件类型</h2><div class="language-typescript line-numbers-mode" data-ext="ts"><pre class="language-typescript"><code>ClipboardEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 剪贴板事件对象
+
+DragEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 拖拽事件对象
+
+ChangeEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> Change 事件对象
+
+KeyboardEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 键盘事件对象
+
+MouseEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 鼠标事件对象
+
+TouchEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 触摸事件对象
+
+WheelEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 滚轮事件对象
+
+AnimationEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 动画事件对象
+
+TransitionEvent<span class="token operator">&lt;</span><span class="token constant">T</span> <span class="token operator">=</span> Element<span class="token operator">&gt;</span> 过渡事件对象
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),p=[o];function l(r,c){return s(),a("div",null,p)}const d=n(t,[["render",l],["__file","eventType.html.vue"]]);export{d as default};
